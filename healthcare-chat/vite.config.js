59af41593b8ecc93eaebc268251e891dfaf99ed0
shaik-mohammed-huzaifa <shaikmohdhuz@gmail.com>
2025-03-24 18:15:00 +0530

    config

--- healthcare-chat/vite.config.js
+++ healthcare-chat/vite.config.js
@@ -11,7 +11,8 @@ export default defineConfig({
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom'],
-          kendo: ['@progress/kendo-react-buttons', '@progress/kendo-react-dateinputs', '@progress/kendo-react-dropdowns', '@progress/kendo-react-inputs', '@progress/kendo-react-intl', '@progress/kendo-react-layouts', '@progress/kendo-react-popup', '@progress/kendo-react-progressbars', '@progress/kendo-react-dateinputs', '@progress/kendo-react-grid', '@progress/kendo-react-notification', '@progress/kendo-react-dialogs']
+          kendo: ['@progress/kendo-react-buttons', '@progress/kendo-react-dateinputs', '@progress/kendo-react-dropdowns', '@progress/kendo-react-inputs', '@progress/kendo-react-intl', '@progress/kendo-react-layout', '@progress/kendo-react-popup', '@progress/kendo-react-progressbars', '@progress/kendo-react-dateinputs', '@progress/kendo-react-grid', '@progress/kendo-react-notification', '@progress/kendo-react-dialogs'],
+          langchain: ['@langchain/core', '@langchain/openai']
         }
       }
     }
